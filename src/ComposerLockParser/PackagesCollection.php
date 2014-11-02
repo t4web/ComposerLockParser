eefc1598159f8f27f1f7d3ce1b90727f6a7e292e
maxgu <maxgu@t4web.com.ua>
2014-11-02 09:24:25 +0200

    add getByName() and hasPackage() to PackageCollection

--- src/ComposerLockParser/PackagesCollection.php
+++ src/ComposerLockParser/PackagesCollection.php
@@ -6,5 +6,62 @@ use ArrayObject;
 
 class PackagesCollection extends ArrayObject
 {
+    /**
+     * @var array
+     */
+    private $indexedByName;
+
+    /**
+     * @param string $name
+     *
+     * @return Package
+     */
+    public function getByName($name)
+    {
+        if (!$this->hasPackage($name)) {
+            throw new \UnexpectedValueException("Package $name not exists");
+        }
+
+        return $this->getIndexedByName()[$name];
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasPackage($name)
+    {
+        return array_key_exists($name, $this->getIndexedByName());
+    }
+
+    public function offsetSet($index, $package)
+    {
+        if ($package instanceof Package) {
+            $this->indexedByName[$package->getName()] = $package;
+        }
+
+        return parent::offsetSet($index, $package);
+    }
+
+    /**
+     * @return array
+     */
+    private function getIndexedByName()
+    {
+        if (!empty($this->indexedByName)) {
+            return $this->indexedByName;
+        }
+
+        /** @var Package $package */
+        foreach($this->getArrayCopy() as $package) {
+            if (!($package instanceof Package)) {
+                continue;
+            }
+            $this->indexedByName[$package->getName()] = $package;
+        }
+
+        return $this->indexedByName;
+    }
 
 }
--- tests/unit/PackagesCollectionTest.php
+++ tests/unit/PackagesCollectionTest.php
@@ -3,14 +3,54 @@
 namespace ComposerLockParser\UnitTest;
 
 use ComposerLockParser\PackagesCollection;
+use Codeception\Util\Stub;
 
 class PackagesCollectionTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testConstructor()
+    public function testGetByName()
     {
-        $packagesCollection = new PackagesCollection([]);
+        $package1 = Stub::make('ComposerLockParser\Package', array('getName' => 'package1'));
+        $package2 = Stub::make('ComposerLockParser\Package', array('getName' => 'package2'));
+        $package3 = Stub::make('ComposerLockParser\Package', array('getName' => 'package3'));
+        $packagesCollection = new PackagesCollection();
 
+        $packagesCollection[] = $package1;
+        $packagesCollection[] = $package2;
+        $packagesCollection[] = $package3;
+
+        $this->assertTrue($packagesCollection->hasPackage('package1'));
+        $this->assertEquals($package1, $packagesCollection->getByName('package1'));
+
+        $this->assertTrue($packagesCollection->hasPackage('package2'));
+        $this->assertEquals($package2, $packagesCollection->getByName('package2'));
+
+        $this->assertTrue($packagesCollection->hasPackage('package3'));
+        $this->assertEquals($package3, $packagesCollection->getByName('package3'));
+    }
+
+    public function testReIndexingByName()
+    {
+        $package1 = Stub::make('ComposerLockParser\Package', array('getName' => 'package1'));
+        $package2 = Stub::make('ComposerLockParser\Package', array('getName' => 'package2'));
+        $package3 = Stub::make('ComposerLockParser\Package', array('getName' => 'package3'));
+        $packagesCollection = new PackagesCollection();
+
+        $packagesCollection[] = $package1;
+        $packagesCollection[] = $package2;
+
+        $packagesCollection->hasPackage('package1');
+
+        $packagesCollection[] = $package3;
+
+        $this->assertTrue($packagesCollection->hasPackage('package1'));
+        $this->assertEquals($package1, $packagesCollection->getByName('package1'));
+
+        $this->assertTrue($packagesCollection->hasPackage('package2'));
+        $this->assertEquals($package2, $packagesCollection->getByName('package2'));
+
+        $this->assertTrue($packagesCollection->hasPackage('package3'));
+        $this->assertEquals($package3, $packagesCollection->getByName('package3'));
     }
 
 }
